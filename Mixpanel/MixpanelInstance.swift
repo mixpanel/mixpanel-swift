5f2757f902f1275d65c65cad19c3efa2273923a2
bre7 <mobileuy@gmail.com>
2016-09-07 23:46:54 -0300

    Xcode 8 GM fixes (@escaping missing)

--- Mixpanel/MixpanelInstance.swift
+++ Mixpanel/MixpanelInstance.swift
@@ -720,7 +720,7 @@ extension MixpanelInstance {
 extension MixpanelInstance: InAppNotificationsDelegate {
     // MARK: - Decide
 
-    func checkDecide(forceFetch: Bool = false, completion: ((_ response: DecideResponse?) -> Void)) {
+    func checkDecide(forceFetch: Bool = false, completion: @escaping ((_ response: DecideResponse?) -> Void)) {
         guard let distinctId = people.distinctId else {
             Logger.info(message: "Can't fetch from Decide without identifying first")
             return

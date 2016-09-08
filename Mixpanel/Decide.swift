1e21e1aa35374debb87f3a3d22e3f90b3fd0e6b7
Yarden Eitan <yarneo@gmail.com>
2016-09-08 10:22:41 -0700

    fix for hound warning

--- Mixpanel/Decide.swift
+++ Mixpanel/Decide.swift
@@ -30,7 +30,9 @@ class Decide {
         }
     }
 
-    func checkDecide(forceFetch: Bool = false, distinctId: String, token: String, completion: @escaping ((_ response: DecideResponse?) -> Void)) {
+    func checkDecide(forceFetch: Bool = false,
+                     distinctId: String, token: String,
+                     completion: @escaping ((_ response: DecideResponse?) -> Void)) {
         var decideResponse = DecideResponse()
 
         if !decideFetched || forceFetch {

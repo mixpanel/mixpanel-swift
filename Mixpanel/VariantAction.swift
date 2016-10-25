bc76da7c9d65f2d2d8a2abf8edb8a08c6f4eb60f
Yarden Eitan <yarneo@gmail.com>
2016-10-25 12:44:52 -0700

    tvos build working

--- Mixpanel/VariantAction.swift
+++ Mixpanel/VariantAction.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class VariantAction: NSObject, NSCoding {
     let name: String
--- Mixpanel/VariantTweak.swift
+++ Mixpanel/VariantTweak.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class VariantTweak: NSObject, NSCoding {
     let name: String

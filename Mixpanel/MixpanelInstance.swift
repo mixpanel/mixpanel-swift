7a80bf26787f75f59854ef9c218e853bb6a52ff4
Jared McFarland <jared@mixpanel.com>
2016-12-15 16:52:16 +0100

    Fixed whitespace violations, again.

--- Mixpanel/MixpanelInstance.swift
+++ Mixpanel/MixpanelInstance.swift
@@ -40,10 +40,10 @@ open class MixpanelInstance: CustomDebugStringConvertible, FlushDelegate {
 
     /// distinctId string that uniquely identifies the current user.
     open var distinctId = ""
-    
+
     /// alias string that uniquely identifies the current user.
     open var alias = ""
-    
+
     /// Accessor to the Mixpanel People API object.
     open var people: People!
 

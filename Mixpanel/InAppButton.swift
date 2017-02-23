f71e8230a71d79da83044c2333faa03797f3e557
Rob Resma <rob.resma@asurion.com>
2017-02-22 17:58:05 -0800

    Adding payload methods for InAppButton and TakeoverNotificatoin

--- Mixpanel/InAppButton.swift
+++ Mixpanel/InAppButton.swift
@@ -9,7 +9,14 @@
 import Foundation
 
 class InAppButton {
-
+    enum PayloadKey {
+        static let text = "text"
+        static let textColor = "text_color"
+        static let backgroundColor = "bg_color"
+        static let borderColor = "border_color"
+        static let callToActionURL = "cta_url"
+    }
+    
     let text: String
     let textColor: UInt
     let backgroundColor: UInt
@@ -22,28 +29,28 @@ class InAppButton {
             return nil
         }
 
-        guard let text = object["text"] as? String else {
+        guard let text = object[PayloadKey.text] as? String else {
             Logger.error(message: "invalid notification button text")
             return nil
         }
 
-        guard let textColor = object["text_color"] as? UInt else {
+        guard let textColor = object[PayloadKey.textColor] as? UInt else {
             Logger.error(message: "invalid notification button text color")
             return nil
         }
 
-        guard let backgroundColor = object["bg_color"] as? UInt else {
+        guard let backgroundColor = object[PayloadKey.backgroundColor] as? UInt else {
             Logger.error(message: "invalid notification button background color")
             return nil
         }
 
-        guard let borderColor = object["border_color"] as? UInt else {
+        guard let borderColor = object[PayloadKey.borderColor] as? UInt else {
             Logger.error(message: "invalid notification button border color")
             return nil
         }
 
         var callToActionURL: URL?
-        if let URLString = object["cta_url"] as? String {
+        if let URLString = object[PayloadKey.callToActionURL] as? String {
             callToActionURL = URL(string: URLString)
         }
 
@@ -53,5 +60,18 @@ class InAppButton {
         self.borderColor        = borderColor
         self.callToActionURL    = callToActionURL
 
+    }
+    
+    func payload() -> [String: AnyObject] {
+        var payload = [String: AnyObject]()
+        payload[PayloadKey.text] = text as AnyObject
+        payload[PayloadKey.textColor] = textColor as AnyObject
+        payload[PayloadKey.backgroundColor] = backgroundColor as AnyObject
+        payload[PayloadKey.borderColor] = borderColor as AnyObject
+        if let callToActionURLString = callToActionURL?.absoluteString {
+            payload[PayloadKey.callToActionURL] = callToActionURLString as AnyObject
+        }
+        return payload
+        
     }
 }

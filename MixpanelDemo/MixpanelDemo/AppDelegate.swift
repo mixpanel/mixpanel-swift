ba61f98ff26fec059fbb6091b522a5d241464828
Hairuo Sang <hairuo.sang@gmail.com>
2017-07-06 13:07:50 -0700

    removed unneeded changes

--- MixpanelDemo/MixpanelDemo/AppDelegate.swift
+++ MixpanelDemo/MixpanelDemo/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         var ADD_YOUR_MIXPANEL_TOKEN_BELOW_🛠🛠🛠🛠🛠🛠: String
-        Mixpanel.initialize(token: "28abe594236545f6c3496c9375d9c5cb")
+        Mixpanel.initialize(token: "MIXPANEL_TOKEN")
         Mixpanel.mainInstance().loggingEnabled = true
         Mixpanel.mainInstance().flushInterval = 5
         let allTweaks: [TweakClusterType] = [MixpanelTweaks.floatTweak,

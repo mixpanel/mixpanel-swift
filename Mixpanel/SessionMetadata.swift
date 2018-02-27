b5c8ebbfdfc50ae330a11faa2a086c9cdde66982
Zihe Jia <zihe.jia@mixpanel.com>
2018-02-27 15:24:38 -0800

    Change ids in session metadata to 16 hex chars

--- Mixpanel/SessionMetadata.swift
+++ Mixpanel/SessionMetadata.swift
@@ -11,24 +11,25 @@ import Foundation
 class SessionMetadata {
     var eventsCounter: UInt64 = 0
     var peopleCounter: UInt64 = 0
-    var sessionID: UInt64 = 0
+    var sessionID: String = ""
     var sessionStartEpoch: UInt64 = 0
     var trackingQueue: DispatchQueue
 
     init(trackingQueue: DispatchQueue) {
         self.trackingQueue = trackingQueue
     }
+    
     func applicationDidBecomeActive() {
         trackingQueue.async {
             self.eventsCounter = 0
             self.peopleCounter = 0
-            self.sessionID = UInt64.random
+            self.sessionID = String.randomId()
             self.sessionStartEpoch = UInt64(Date().timeIntervalSince1970)
         }
     }
 
     func toDict(isEvent: Bool = true) -> InternalProperties {
-        let dict = ["$mp_event_id": UInt64.random,
+        let dict : [String: Any] = ["$mp_event_id":  String.randomId(),
                     "$mp_session_id": sessionID,
                     "$mp_session_seq_id": (isEvent ? eventsCounter : peopleCounter),
                     "$mp_session_start_sec": sessionStartEpoch]
@@ -37,12 +38,8 @@ class SessionMetadata {
     }
 }
 
-extension UInt64 {
-    static var random: UInt64 {
-        let hex = UUID().uuidString
-            .components(separatedBy: "-")
-            .suffix(2)
-            .joined()
-        return UInt64(hex, radix: 0x10)!
+private extension String {
+    static func randomId() -> String {
+        return String(format: "%08x%08x", arc4random(), arc4random())
     }
 }

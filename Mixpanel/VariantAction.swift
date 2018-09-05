4726d02f9de6bae8d50239dbe2c0a5c99522d950
romanfurman6 <roman.furman@uptech.team>
2018-09-05 13:51:34 +0300

    add swift 4.2 support

--- Mixpanel/VariantAction.swift
+++ Mixpanel/VariantAction.swift
@@ -285,13 +285,13 @@ class VariantAction: NSObject, NSCoding {
 
     class func extractAndRunMethodFromSelector(selector: Selector, implementation: IMP?, object: AnyObject, args: [Any]) -> AnyObject? {
         if selector.description == "setImage:forState:" {
-            typealias Function = @convention(c) (AnyObject, Selector, UIImage, UIControlState) -> Void
+            typealias Function = @convention(c) (AnyObject, Selector, UIImage, UIControl.State) -> Void
             let function = unsafeBitCast(implementation, to: Function.self)
-            function(object, selector, args[0] as! UIImage, UIControlState(rawValue: args[1] as! UInt))
+            function(object, selector, args[0] as! UIImage, UIControl.State(rawValue: args[1] as! UInt))
         } else if selector.description == "imageForState:" {
-            typealias Function = @convention(c) (AnyObject, Selector, UIControlState) -> Unmanaged<UIImage>
+            typealias Function = @convention(c) (AnyObject, Selector, UIControl.State) -> Unmanaged<UIImage>
             let function = unsafeBitCast(implementation, to: Function.self)
-            let val = function(object, #selector(UIButton.image(for:)), UIControlState(rawValue: args[0] as! UInt)).takeUnretainedValue()
+            let val = function(object, #selector(UIButton.image(for:)), UIControl.State(rawValue: args[0] as! UInt)).takeUnretainedValue()
             return val
         } else if selector.description == "setFrame:" {
             guard let nsValue = args[0] as? NSValue else {
@@ -318,17 +318,17 @@ class VariantAction: NSObject, NSCoding {
             let function = unsafeBitCast(implementation, to: Function.self)
             function(object, selector, args[0] as! Bool)
         } else if selector.description == "setBackgroundImage:forState:" {
-            typealias Function = @convention(c) (AnyObject, Selector, UIImage, UIControlState) -> Void
+            typealias Function = @convention(c) (AnyObject, Selector, UIImage, UIControl.State) -> Void
             let function = unsafeBitCast(implementation, to: Function.self)
-            function(object, selector, args[0] as! UIImage, UIControlState(rawValue: args[1] as! UInt))
+            function(object, selector, args[0] as! UIImage, UIControl.State(rawValue: args[1] as! UInt))
         } else if selector.description == "setTextAlignment:" {
             typealias Function = @convention(c) (AnyObject, Selector, NSTextAlignment) -> Void
             let function = unsafeBitCast(implementation, to: Function.self)
             function(object, selector, NSTextAlignment(rawValue: args[0] as! Int)!)
         } else if selector.description == "setTitle:forState:" {
-            typealias Function = @convention(c) (AnyObject, Selector, NSString, UIControlState) -> Void
+            typealias Function = @convention(c) (AnyObject, Selector, NSString, UIControl.State) -> Void
             let function = unsafeBitCast(implementation, to: Function.self)
-            function(object, selector, args[0] as! NSString, UIControlState(rawValue: args[1] as! UInt))
+            function(object, selector, args[0] as! NSString, UIControl.State(rawValue: args[1] as! UInt))
         }
         return nil
     }

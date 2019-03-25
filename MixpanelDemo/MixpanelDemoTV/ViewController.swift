e94c641697c8fc701b32244b5a899e0d8ce1cf1a
Zihe Jia <zihe.jia@mixpanel.com>
2019-03-24 23:11:52 -0700

    add apple tv samples

---
+++ MixpanelDemo/MixpanelDemoTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MixpanelDemoTV
+//
+//  Created by Zihe Jia on 3/22/19.
+//  Copyright © 2019 Mixpanel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

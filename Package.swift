681eb3617824caecbf73f6645fc5b12cf6a01f67
Zihe Jia <zihe.jia@mixpanel.com>
2020-10-15 09:30:07 -0700

    SPM: add conditional swift settings for check decide

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
                 .process("placeholder-image.png"),
             ],
             swiftSettings: [
-                .define("DECIDE")
+                .define("DECIDE", .when(platforms: [.iOS])),
             ]
         ),
     ]

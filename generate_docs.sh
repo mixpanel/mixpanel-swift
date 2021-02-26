6c8deb5b0f3d2ea4210e62de82b5bec7980deb7f
Jared McFarland <jared@mixpanel.com>
2021-02-26 10:55:45 -0800

    Version 2.8.4

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,6 +3,6 @@ jazzy \
 -a Mixpanel \
 -u http://mixpanel.com \
 --github_url https://github.com/mixpanel/mixpanel-swift \
---module-version 2.8.3 \
+--module-version 2.8.4 \
 --framework-root . \
 --module Mixpanel

2fc592a18d29275b870c76202951c1570eb882a3
Zihe Jia <zihe.jia@mixpanel.com>
2021-04-20 12:13:54 -0700

    Version 2.9.3

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,6 +3,6 @@ jazzy \
 -a Mixpanel \
 -u http://mixpanel.com \
 --github_url https://github.com/mixpanel/mixpanel-swift \
---module-version 2.9.0 \
+--module-version 2.9.3 \
 --framework-root . \
 --module Mixpanel

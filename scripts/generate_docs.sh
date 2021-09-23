9bd3a7dae1c3f987ab67a9cdfbb38f3e1d798ab5
Zihe Jia <zihe.jia@mixpanel.com>
2021-09-23 14:56:17 -0700

    Version 3.0.0.beta.5

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,6 +3,6 @@ jazzy \
 -a Mixpanel \
 -u http://mixpanel.com \
 --github_url https://github.com/mixpanel/mixpanel-swift \
---module-version 3.0.0.beta.4 \
+--module-version 3.0.0.beta.5 \
 --framework-root . \
 --module Mixpanel

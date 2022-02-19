4be179df09e73ebf1e20e07c6e346bb21d97a77d
Zihe Jia <36679208+zihejia@users.noreply.github.com>
2022-02-18 17:04:29 -0800

    Update doc for `instanceName`
    Co-authored-by: Jared McFarland <jared@mixpanel.com>

--- Sources/Mixpanel.swift
+++ Sources/Mixpanel.swift
@@ -62,7 +62,7 @@ open class Mixpanel {
 
      - parameter token:                     your project token
      - parameter flushInterval:             Optional. Interval to run background flushing
-     - parameter instanceName:              Optional. The name you want to call this instance, has to be unique and correspond with the instance's project token
+     - parameter instanceName:              Optional. The name you want to call this instance, must be unique 1:1 for each instance's project token. Defaults to project token.
      - parameter optOutTrackingByDefault:   Optional. Whether or not to be opted out from tracking by default
      - parameter useUniqueDistinctId:       Optional. whether or not to use the unique device identifier as the distinct_id
      - parameter superProperties:           Optional. Super properties dictionary to register during initialization

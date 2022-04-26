b7c49fe87da4cc44c2986bf91f3419fb03eca1ae
Jared McFarland <jared@mixpanel.com>
2022-04-26 14:22:41 -0700

    reverse if logic

--- Sources/MixpanelPersistence.swift
+++ Sources/MixpanelPersistence.swift
@@ -86,11 +86,12 @@ class MixpanelPersistence {
         if excludeAutomaticEvents && type == .events {
             entities = entities.filter { !($0["event"] as! String).hasPrefix("$ae_") }
         }
-        let distinctId = MixpanelPersistence.loadIdentity(apiToken: apiToken).distinctID
-        if type != PersistenceType.people {
-            return entities
+        
+        if type == PersistenceType.people {
+            let distinctId = MixpanelPersistence.loadIdentity(apiToken: apiToken).distinctID
+            return entities.map { entityWithDistinctId($0, distinctId: distinctId) }
         }
-        return entities.map { entityWithDistinctId($0, distinctId: distinctId) }
+        return entities
     }
 
     private func entityWithDistinctId(_ entity: InternalProperties, distinctId: String) -> InternalProperties {

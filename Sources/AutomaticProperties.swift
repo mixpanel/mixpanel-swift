8c245c80440efe92de98a4176dd23d0476920748
Jared McFarland <jared@mixpanel.com>
2022-07-18 16:14:04 -0700

    Version 3.5.1

--- Sources/AutomaticProperties.swift
+++ Sources/AutomaticProperties.swift
@@ -145,7 +145,7 @@ class AutomaticProperties {
     }
 
     class func libVersion() -> String {
-        return "3.5.0"
+        return "3.5.1"
     }
 
 }
--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,6 +3,6 @@ jazzy \
 -a Mixpanel \
 -u http://mixpanel.com \
 --github_url https://github.com/mixpanel/mixpanel-swift \
---module-version 3.5.0 \
+--module-version 3.5.1 \
 --framework-root . \
 --module Mixpanel

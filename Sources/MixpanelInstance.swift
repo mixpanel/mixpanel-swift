3928e1cb7a7a83d00a8bd27f29038ce96c6fd09e
deepak neralla <deepak.neralla@gmail.com>
2023-02-14 16:45:18 -0800

    identity v3 changes for swift sdk

--- Sources/MixpanelInstance.swift
+++ Sources/MixpanelInstance.swift
@@ -431,6 +431,7 @@ open class MixpanelInstance: CustomDebugStringConvertible, FlushDelegate, AEDele
 #endif
     
     func defaultDistinctId() -> String {
+        let prefix = "$device:"
         let distinctId: String?
         if useUniqueDistinctId {
             distinctId = uniqueIdentifierForDevice()
@@ -441,7 +442,8 @@ open class MixpanelInstance: CustomDebugStringConvertible, FlushDelegate, AEDele
             distinctId = nil
 #endif
         }
-        return distinctId ?? UUID().uuidString // use a random UUID by default
+        let ret =  distinctId ?? UUID().uuidString // use a random UUID by default
+        return prefix + ret
     }
     
     func uniqueIdentifierForDevice() -> String? {

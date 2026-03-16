a7010e40bd5e89514989dfd0bfcfb83966562b7f
Ketan S <ketan.shikhare@mixpanel.com>
2026-03-17 00:00:43 +0530

    Updated generate_docs script
    Updated script to build with the jsonlogic library

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,8 +1,27 @@
+#!/bin/sh
+set -e
+
+DERIVED_DATA="$PWD/build"
+
+echo "==> Building full scheme to compile SPM dependencies..."
+xcodebuild build \
+  -scheme Mixpanel \
+  -destination 'generic/platform=iOS' \
+  -derivedDataPath "$DERIVED_DATA" \
+  CODE_SIGN_IDENTITY="" \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGNING_ALLOWED=NO
+
+echo "==> Generating docs..."
 jazzy \
---clean \
--a Mixpanel \
--u http://mixpanel.com \
---github_url https://github.com/mixpanel/mixpanel-swift \
---module-version 5.2.0 \
---framework-root . \
---module Mixpanel
+  --clean \
+  -a Mixpanel \
+  -u http://mixpanel.com \
+  --github_url https://github.com/mixpanel/mixpanel-swift \
+  --module-version 5.2.0 \
+  --framework-root . \
+  --module Mixpanel \
+  --xcodebuild-arguments \
+    -scheme,Mixpanel,\
+    -destination,'generic/platform=iOS',\
+    -derivedDataPath,"$DERIVED_DATA"	

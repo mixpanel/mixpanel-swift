a0ee60cee260731a2c0e53a65db534ff5b3c148c
Ketan S <ketan.shikhare@mixpanel.com>
2026-04-07 12:04:09 +0530

    Lower mixpanel-swift-common min version and remove duplicate
    Reduce the XCRemoteSwiftPackageReference minimumVersion for mixpanel-swift-common from 2.0.0 to 1.0.0 in Mixpanel.xcodeproj to allow older compatible releases. Also remove a duplicated flagManager.checkFirstTimeEvents(...) block in Sources/Track.swift to prevent the same first-time event check from running twice.

--- Sources/Track.swift
+++ Sources/Track.swift
@@ -96,12 +96,6 @@ class Track {
       flagManager.checkFirstTimeEvents(eventName: ev, properties: p)
     }
 
-    // Check for first-time event matches
-    if let mixpanelInstance = mixpanelInstance,
-       let flagManager = mixpanelInstance.flags as? FeatureFlagManager {
-      flagManager.checkFirstTimeEvents(eventName: ev, properties: p)
-    }
-
     var trackEvent: InternalProperties = ["event": ev, "properties": p]
     metadata.toDict().forEach { (k, v) in trackEvent[k] = v }
 

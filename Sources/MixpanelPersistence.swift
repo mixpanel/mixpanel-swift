209e627c2e82bccfde36ffc33c5ae52489d95ccb
Tyler Roach <tyler.roach@mixpanel.com>
2026-05-05 15:38:55 -0400

    remove unused code

--- Sources/MixpanelPersistence.swift
+++ Sources/MixpanelPersistence.swift
@@ -47,7 +47,6 @@ struct MixpanelUserDefaultsKeys {
   static let userID = "MPUserId"
   static let alias = "MPAlias"
   static let hadPersistedDistinctId = "MPHadPersistedDistinctId"
-  static let flags = "MPFlags"
   static let flagsPersistence = "MPFlagsPersistence"
 }
 
@@ -246,41 +245,6 @@ class MixpanelPersistence {
         ?? InternalProperties()
     } catch {
       MixpanelLogger.warn(message: "Failed to unarchive super properties")
-      return InternalProperties()
-    }
-  }
-
-  /// -- Feature Flags --
-  /// NOT currently used
-
-  static func saveFlags(flags: InternalProperties, instanceName: String) {
-    guard let defaults = UserDefaults(suiteName: MixpanelUserDefaultsKeys.suiteName) else {
-      return
-    }
-    let prefix = "\(MixpanelUserDefaultsKeys.prefix)-\(instanceName)-"
-    do {
-      let flagsData = try NSKeyedArchiver.archivedData(
-        withRootObject: flags, requiringSecureCoding: false)
-      defaults.set(flagsData, forKey: "\(prefix)\(MixpanelUserDefaultsKeys.flags)")
-      defaults.synchronize()
-    } catch {
-      MixpanelLogger.warn(message: "Failed to archive flags")
-    }
-  }
-
-  static func loadFlags(instanceName: String) -> InternalProperties {
-    guard let defaults = UserDefaults(suiteName: MixpanelUserDefaultsKeys.suiteName) else {
-      return InternalProperties()
-    }
-    let prefix = "\(MixpanelUserDefaultsKeys.prefix)-\(instanceName)-"
-    guard let flags = defaults.data(forKey: "\(prefix)\(MixpanelUserDefaultsKeys.flags)") else {
-      return InternalProperties()
-    }
-    do {
-      return try NSKeyedUnarchiver.unarchivedObject(ofClasses: archivedClasses, from: flags)
-        as? InternalProperties ?? InternalProperties()
-    } catch {
-      MixpanelLogger.warn(message: "Failed to unarchive flags")
       return InternalProperties()
     }
   }

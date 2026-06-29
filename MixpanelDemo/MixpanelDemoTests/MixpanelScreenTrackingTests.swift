50eee4816564ba558ae33859961dfe0d61451c7e
Rahul Raveendran V P <rahul.raveendran@mixpanel.com>
2026-06-29 13:28:49 +0530

    test: add $mp_autocapture assertions and missing test coverage
    Add $mp_autocapture assertion to all existing screen tracking tests.
    Add testScreenLeaveWithoutProperties and testSdkPropertiesCannotBeOverridden
    to verify property merging and that SDK properties are not overridable.

--- MixpanelDemo/MixpanelDemoTests/MixpanelScreenTrackingTests.swift
+++ MixpanelDemo/MixpanelDemoTests/MixpanelScreenTrackingTests.swift
@@ -26,6 +26,7 @@ class MixpanelScreenTrackingTests: MixpanelBaseTests {
 
         let props = event?["properties"] as? InternalProperties
         XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["$mp_autocapture"] as? Bool, true)
         XCTAssertEqual(props?["extra_prop"] as? String, "extra_value")
         XCTAssertNotNil(props?["$screen_height"])
 
@@ -46,6 +47,7 @@ class MixpanelScreenTrackingTests: MixpanelBaseTests {
 
         let props = event?["properties"] as? InternalProperties
         XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["$mp_autocapture"] as? Bool, true)
 
         removeDBfile(testMixpanel.apiToken)
     }
@@ -65,7 +67,47 @@ class MixpanelScreenTrackingTests: MixpanelBaseTests {
 
         let props = event?["properties"] as? InternalProperties
         XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["$mp_autocapture"] as? Bool, true)
         XCTAssertEqual(props?["time_spent"] as? Int, 30)
+
+        removeDBfile(testMixpanel.apiToken)
+    }
+
+    func testScreenLeaveWithoutProperties() {
+        let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
+        testMixpanel.autocapture.trackScreenLeave(screenName: "HomeScreen")
+
+        waitForTrackingQueue(testMixpanel)
+
+        let events = eventQueue(token: testMixpanel.apiToken)
+        XCTAssertEqual(events.count, 1)
+
+        let event = events.first
+        XCTAssertEqual(event?["event"] as? String, "$mp_page_leave")
+
+        let props = event?["properties"] as? InternalProperties
+        XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["$mp_autocapture"] as? Bool, true)
+
+        removeDBfile(testMixpanel.apiToken)
+    }
+
+    func testSdkPropertiesCannotBeOverridden() {
+        let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
+        let properties: Properties = [
+            "current_page_title": "SpoofedTitle",
+            "$mp_autocapture": false,
+        ]
+        testMixpanel.autocapture.trackScreenView(screenName: "HomeScreen", properties: properties)
+
+        waitForTrackingQueue(testMixpanel)
+
+        let events = eventQueue(token: testMixpanel.apiToken)
+        XCTAssertEqual(events.count, 1)
+
+        let props = events.first?["properties"] as? InternalProperties
+        XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["$mp_autocapture"] as? Bool, true)
 
         removeDBfile(testMixpanel.apiToken)
     }

761d1fddfd871e28557a188f52b1dc1155a13c17
Rahul Raveendran V P <rahul.raveendran@mixpanel.com>
2026-06-29 12:45:51 +0530

    fix: update Mac and TV test targets to use autocapture namespace and apply swift-format

--- MixpanelDemo/MixpanelDemoMacTests/MixpanelScreenTrackingTests.swift
+++ MixpanelDemo/MixpanelDemoMacTests/MixpanelScreenTrackingTests.swift
@@ -11,62 +11,62 @@ import XCTest
 
 class MixpanelScreenTrackingTests: MixpanelBaseTests {
 
-  func testScreenView() {
-    let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
-    let properties: Properties = ["extra_prop": "extra_value"]
-    testMixpanel.trackScreenView(screenName: "HomeScreen", properties: properties)
+    func testScreenView() {
+        let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
+        let properties: Properties = ["extra_prop": "extra_value"]
+        testMixpanel.autocapture.trackScreenView(screenName: "HomeScreen", properties: properties)
 
-    waitForTrackingQueue(testMixpanel)
+        waitForTrackingQueue(testMixpanel)
 
-    let events = eventQueue(token: testMixpanel.apiToken)
-    XCTAssertEqual(events.count, 1)
+        let events = eventQueue(token: testMixpanel.apiToken)
+        XCTAssertEqual(events.count, 1)
 
-    let event = events.first
-    XCTAssertEqual(event?["event"] as? String, "$mp_page_view")
+        let event = events.first
+        XCTAssertEqual(event?["event"] as? String, "$mp_page_view")
 
-    let props = event?["properties"] as? InternalProperties
-    XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
-    XCTAssertEqual(props?["extra_prop"] as? String, "extra_value")
-    XCTAssertNotNil(props?["$screen_height"])
+        let props = event?["properties"] as? InternalProperties
+        XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["extra_prop"] as? String, "extra_value")
+        XCTAssertNotNil(props?["$screen_height"])
 
-    removeDBfile(testMixpanel.apiToken)
-  }
+        removeDBfile(testMixpanel.apiToken)
+    }
 
-  func testScreenViewWithoutProperties() {
-    let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
-    testMixpanel.trackScreenView(screenName: "HomeScreen")
+    func testScreenViewWithoutProperties() {
+        let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
+        testMixpanel.autocapture.trackScreenView(screenName: "HomeScreen")
 
-    waitForTrackingQueue(testMixpanel)
+        waitForTrackingQueue(testMixpanel)
 
-    let events = eventQueue(token: testMixpanel.apiToken)
-    XCTAssertEqual(events.count, 1)
+        let events = eventQueue(token: testMixpanel.apiToken)
+        XCTAssertEqual(events.count, 1)
 
-    let event = events.first
-    XCTAssertEqual(event?["event"] as? String, "$mp_page_view")
+        let event = events.first
+        XCTAssertEqual(event?["event"] as? String, "$mp_page_view")
 
-    let props = event?["properties"] as? InternalProperties
-    XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        let props = event?["properties"] as? InternalProperties
+        XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
 
-    removeDBfile(testMixpanel.apiToken)
-  }
+        removeDBfile(testMixpanel.apiToken)
+    }
 
-  func testScreenLeave() {
-    let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
-    let properties: Properties = ["time_spent": 30]
-    testMixpanel.trackScreenLeave(screenName: "HomeScreen", properties: properties)
+    func testScreenLeave() {
+        let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: false)
+        let properties: Properties = ["time_spent": 30]
+        testMixpanel.autocapture.trackScreenLeave(screenName: "HomeScreen", properties: properties)
 
-    waitForTrackingQueue(testMixpanel)
+        waitForTrackingQueue(testMixpanel)
 
-    let events = eventQueue(token: testMixpanel.apiToken)
-    XCTAssertEqual(events.count, 1)
+        let events = eventQueue(token: testMixpanel.apiToken)
+        XCTAssertEqual(events.count, 1)
 
-    let event = events.first
-    XCTAssertEqual(event?["event"] as? String, "$mp_page_leave")
+        let event = events.first
+        XCTAssertEqual(event?["event"] as? String, "$mp_page_leave")
 
-    let props = event?["properties"] as? InternalProperties
-    XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
-    XCTAssertEqual(props?["time_spent"] as? Int, 30)
+        let props = event?["properties"] as? InternalProperties
+        XCTAssertEqual(props?["current_page_title"] as? String, "HomeScreen")
+        XCTAssertEqual(props?["time_spent"] as? Int, 30)
 
-    removeDBfile(testMixpanel.apiToken)
-  }
+        removeDBfile(testMixpanel.apiToken)
+    }
 }

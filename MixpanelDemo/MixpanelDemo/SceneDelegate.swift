e0fcd8a89d4d8937ed21c9a409d8fed20149634a
Ketan S <ketan.shikhare@mixpanel.com>
2026-08-21 14:27:46 +0530

    Added scene delegate functionality

---
+++ MixpanelDemo/MixpanelDemo/SceneDelegate.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+
+        // The window will be set up from the Main storyboard automatically
+        // This ensures the storyboard-based UI is properly loaded
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}

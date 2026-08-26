033f9d03ab2a9e1297d0ea43b00c7f2afdc2adee
ketanmixpanel <ketan.shikhare@mixpanel.com>
2026-08-26 12:35:59 +0530

    fix: Added scene delegate functionality to sample app (#769)
    * Added scene delegate functionality
    
    * Update SceneDelegate.swift
    
    * updated formatting

---
+++ MixpanelDemo/MixpanelDemo/SceneDelegate.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        // When using storyboards, the window and root view controller are created automatically
+        // from the storyboard specified in Info.plist's UISceneStoryboardFile
+        guard (scene as? UIWindowScene) != nil else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
